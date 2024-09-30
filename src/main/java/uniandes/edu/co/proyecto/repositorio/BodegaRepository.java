0a9128d56f29c2cd6a3df40f0e4c83c6a02844d0
Nicolás Ballén <n.ballen1@uniandes.edu.co>
2024-09-30 12:02:59 -0500

    repositories Nicolas

---
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/BodegaRepository.java
@@ -0,0 +1,43 @@
+package uniandes.edu.co.proyecto.repositorio;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import uniandes.edu.co.proyecto.modelo.Bodega;
+
+public interface BodegaRepository extends JpaRepository<Bodega, Integer>{
+    @Query(value = "SELECT * FROM bodegas", nativeQuery = true)
+    Collection<Bodega> darBodegas();
+
+    
+    @Query(value = "SELECT * FROM bodegas WHERE id = :id", nativeQuery = true)
+    Bodega darBodega(@Param("id") Integer id);
+
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM bodegas WHERE id = : id", nativeQuery = true)
+    void eliminarBodega(@Param("id") Integer id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM bodegas WHERE sucursal = :sucursal", nativeQuery = true)
+    void eliminarBodegaPorSucursal(@Param("sucursal") Integer sucursal);
+
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE bodegas SET nombre = :nombre, tamaño = :tamaño, sucursal = :sucursal WHERE id = :id", nativeQuery = true)
+    void actualizarBodega(@Param("nombre") String nombre, @Param("tamaño") Integer tamaño, @Param("sucursal") Integer sucursal);
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO bodegas (nombre, tamaño, sucursal) VALUES ( proyecto_sequence.nextval , :nombre, :tamaño, :sucursal)", nativeQuery = true)
+    void insertarBodega(@Param("nombre") String nombre, @Param("tamaño") Integer tamaño, @Param("sucursal") Integer sucursal);
+}
---
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/CategoriaRepository.java
@@ -0,0 +1,38 @@
+package uniandes.edu.co.proyecto.repositorio;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import uniandes.edu.co.proyecto.modelo.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+    @Query(value = "SELECT * FROM categorias", nativeQuery = true)
+    Collection<Categoria> darCategorias();
+
+    
+    @Query(value = "SELECT * FROM categorias WHERE codigo = :codigo", nativeQuery = true)
+    Categoria darCategoria(@Param("codigo") Integer codigo);
+
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM categorias WHERE codigo = : codigo", nativeQuery = true)
+    void eliminarCategoria(@Param("codigo") Integer codigo);
+
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE categorias SET nombre = :nombre, descripcion = :descripcion, caracteristicas = :caracteristicas WHERE codigo = :codigo", nativeQuery = true)
+    void actualizarCategoria(@Param("nombre") String nombre, @Param("descripcion") String descripcion, @Param("caracteristicas") String caracteristicas);
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO categorias (nombre, descripcion, sucursalo) VALUES ( proyecto_sequence.nextval , :nombre, :descripcion, :sucursal)", nativeQuery = true)
+    void insertarCategoria(@Param("nombre") String nombre, @Param("descripcion") String descripcion, @Param("caracteristicas") String caracteristicas);
+}
