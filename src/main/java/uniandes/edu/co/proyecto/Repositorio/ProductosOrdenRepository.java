8740442629fe713bca3af7f17e481ed19908dd80
Nicolás Ballén <n.ballen1@uniandes.edu.co>
2024-09-30 20:21:26 -0500

    arreglando

---
+++ src/main/java/uniandes/edu/co/proyecto/Repositorio/ProductosOrdenRepository.java
@@ -0,0 +1,39 @@
+package uniandes.edu.co.proyecto.repositorio;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import uniandes.edu.co.proyecto.modelo.ProductosOrden;
+import uniandes.edu.co.proyecto.modelo.ProductosOrdenPK;
+
+public interface ProductosOrdenRepository extends JpaRepository<ProductosOrden, ProductosOrdenPK>{
+    @Query(value = "SELECT * FROM productosOrden", nativeQuery = true)
+        Collection<ProductosOrden> darProductosOrdens();
+
+    
+    @Query(value = "SELECT * FROM productosOrden WHERE orden = :orden AND producto = :producto", nativeQuery = true)
+    ProductosOrden darProductosOrden(@Param("orden") Long orden, @Param("producto") Long producto);
+
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM productosOrden WHERE orden = :orden AND producto = :producto", nativeQuery = true)
+    void eliminarProductosOrden(@Param("orden") Long orden, @Param("producto") Long producto);
+
+    
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE productosOrden SET cantidad = :cantidad, precio_acordado = :precio_acordado WHERE orden = :orden AND producto = :producto", nativeQuery = true)
+    void actualizarProductosOrden(@Param("orden") Long orden, @Param("producto") Long producto, @Param("cantidad") Long cantidad, @Param("precio_acordado") Long precio_acordado);
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO productosOrden (orden, producto, cantidad, precio_acordado) VALUES ( :bodega, :producto, :capacidad, :cantidad, :costo_promedio, :nivel_minimo)", nativeQuery = true)
+    void insertarProductosOrden(@Param("orden") Long orden, @Param("producto") Long producto, @Param("cantidad") Long cantidad, @Param("precio_acordado") Long precio_acordado);
+}
